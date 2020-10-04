4eecd774a0fef07bd7eb9dd288d216bea6bdcfc9
UDay Kiran Reddy <ukreddy@erwin.com>
2020-10-04 16:37:27 +0530

    Custom component, single github user display

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 class App extends Component {
   render(){
     return(
       <div className="App">
-        <Navbar title="Github Finder" icon='fab fa-github'/>
+        <Navbar/>
+        <UserItem/>
       </div>
     );
   }
